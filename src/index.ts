a2d9ff73baa23459d2e79dafbe67643818713f6e
JudeEdemNyahe <97879372+JudeEdemNyahe@users.noreply.github.com>
2022-12-19 19:25:54 +0000

    definining our graphql schema

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
 import { ApolloServer } from "apollo-server";
 import { typeDefs } from "./schema";
 import { Query } from "./resolvers/index";
+import { Prisma, PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export interface Context {
+  prisma: PrismaClient<
+    Prisma.PrismaClientOptions,
+    never,
+    Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined
+  >;
+}
+
 const resolvers = {
   Query,
 };
-
+const context = {
+  prisma,
+};
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context,
 });
 
 server.listen().then(({ url }) => {
--- src/schema.ts
+++ src/schema.ts
@@ -4,4 +4,46 @@ export const typeDefs = gql`
   type Query {
     hello: String!
   }
+
+  #Rule 13:Prefix mutation Names with Object for Alphabetical grouping
+  type Mutation {
+    postCreate(title: String!, content: String!): PostPayload!
+  }
+
+  #rule 15:Mutations should provide user business level Errors
+
+  #Rule 1:Describe entity and their relationship
+  type Post {
+    id: ID!
+    title: String!
+    content: String!
+    createdSt: String!
+    published: Boolean!
+    user: User!
+  }
+
+  #password is excluded, #Rule2-never expose implementation details
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    profile: Profile
+    posts: [Post!]!
+  }
+
+  type Profile {
+    id: ID!
+    bio: String
+    user: User!
+  }
+
+  #error handling and return types
+  type UserError {
+    message: String!
+  }
+
+  type PostPayload {
+    userErrors: [UserError!]!
+    Post: Post #must be null to avoid error conflictions
+  }
 `;
